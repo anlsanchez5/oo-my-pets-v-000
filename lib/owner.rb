0effe90eed82955efa79c9281b9f40c01e1adbd4
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-12 01:14:32 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
+
+require 'pry'
 class Owner
   # code goes here
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@all = []
+
+  def initialize(species, name = nil)
+    @species = species
+    @name = nil
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+
 end
